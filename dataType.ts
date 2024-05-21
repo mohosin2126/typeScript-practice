1fecf28f80e4ce17c997ca0702f2b721af63bcdd
Mohosin212601~ <mohosin2126@gmail.com>
2024-05-21 23:48:17 +0600

    next tuple

---
+++ dataType.ts
@@ -0,0 +1,26 @@
+/**Basic Data Types (Primitive)*/
+
+// ---string--
+let firstName:string="name"
+
+// --number--
+let roll:number=124
+
+// --boolean--
+let isAdmin:boolean =true
+
+// undefined 
+let x:undefined =undefined
+
+// null 
+let y:null =null 
+
+
+
+/**Basic Data Types (non-Primitive)*/
+
+// array 
+
+let friends : string []=["hii","hello"]
+
+let eligibleRoll : number []=[123,456]
