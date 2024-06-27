a19d470caaac9709cf82702d0346174f6af3b129
Mohosin2126 <mohosin2126@gmail.com>
2024-06-27 15:54:11 +0600

    type and interface done

---
+++ typescript/src/type&Interface.ts
@@ -0,0 +1,37 @@
+{
+    // Interface
+
+    interface User{
+        name:string;
+        contactNo:string;
+        address:string;
+    }
+    const user:User={
+        name:"mohosin",
+        contactNo:"01770000000",
+        address:"dhaka"
+    }
+
+
+
+    // type
+    type User1={
+        name:string;
+        contactNo:string;
+        address:string;
+    }
+    const user1:User1={
+        name:"mohosin",
+        contactNo:"01770000000",
+        address:"dhaka"
+    }
+
+
+
+
+
+
+
+
+    // 
+}
